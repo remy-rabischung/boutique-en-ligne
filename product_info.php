a8025395d753fcc1ecdea1cc94ddc1fbfbbd91e0
Rémy R <remy.rabischung@laplateforme.io>
2024-07-03 15:38:39 +0200

    Amelioration page product-info

--- product_info.php
+++ product_info.php
@@ -1,57 +1,64 @@
 <?php
 require_once("inc/init.php");
 
-    //////////////////////////////////////////
-    //////////// GET PRODUCT INFOS ///////////
-    //////////////////////////////////////////
+//////////////////////////////////////////
+/////////// GET PRODUCT INFOS ///////////
+//////////////////////////////////////////
 
-    if(isset($_GET['id_product'])) {
-        $id_product = $_GET['id_product'];
-        $stmt = $pdo->query("SELECT * FROM products WHERE id_product = $id_product");
-        $produit = $stmt->fetch(PDO::FETCH_ASSOC);
-        }
+$produit = null;
+if(isset($_GET['id_product'])) {
+    $id_product = $_GET['id_product'];
+    $sql = "SELECT * FROM products WHERE id_product = :id_product";
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindValue(':id_product', $id_product, PDO::PARAM_INT);
+    $stmt->execute();
+    $produit = $stmt->fetch(PDO::FETCH_ASSOC);
+}
 
-    
-    
 require_once("inc/header.php");
 
 ?>
 
-<div class="card col-md-4">
-    <img src="<?= $produit['picture']; ?>" class="card-img-top" alt="Black t-shirt">
-    <div class="card-body">
-        <h5 class="card-title"><?= $produit['title']; ?></h5>
-        <p class="card-text"><?= $produit['description']; ?></p>
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-md-6">
+            <div class="card">
+                <img src="<?= $produit['picture']; ?>" class="card-img-top img-fluid" alt="<?= $produit['title']; ?>">
+                <div class="card-body">
+                    <h5 class="card-title"><?= $produit['title']; ?></h5>
+                    <p class="card-text"><?= $produit['description']; ?></p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-6">
+            <ul class="list-group">
+                <li class="list-group-item">Catégorie : <?= $produit['category']; ?></li>
+                <li class="list-group-item">Prix : <?= $produit['price']; ?> €</li>
+
+                <form method="POST" action="cart.php">
+                    <input type="hidden" name="id_product" value="<?= $produit['id_product']; ?>"/>
+                    <input type="hidden" name="category" value="<?= $produit['category']; ?>"/>
+                    <li class="list-group-item">
+                        <label for="selectQuantity">Quantité :</label>
+                        <select name="quantity" class="custom-select" id="selectQuantity">
+                            <option selected disabled>Choisir la quantité</option>
+                            <?php for($i = 1; $i <= $produit['stock']; $i++) { ?>
+                                <option value="<?= $i ?>"><?= $i ?></option>
+                            <?php } ?>
+                        </select>
+                    </li>
+
+                    <input name="add_product" type="submit" id="addToCart" class="btn btn-primary mt-3" disabled value="Ajouter au panier">
+
+                </form>
+
+            </ul>
+        </div>
     </div>
 </div>
 
-<div class="col-md-4">
-    <ul class="list-group">
-        <li class="list-group-item">Catégorie : <?= $produit['category']; ?></li>
-        <li class="list-group-item">Prix : <?= $produit['price']; ?></li>
-
-        <form method="POST" action="cart.php">
-            <input type="hidden" name="id_product" value="<?= $produit['id_product']; ?>"/>
-            <input type="hidden" name="category" value="<?= $produit['category']; ?>"/>
-            <li class="list-group-item">
-                <p>Quantity :</p>
-                <select name="quantity" class="custom-select" id="selectQuantity">
-                    <option selected="">Choisir la quantité</option>
-                    
-                    <?php for($i = 1; $i <= $produit['stock']; $i++) { ?>
-                        <option value="<?= $i ?>"><?= $i ?></option>
-                    <?php } ?>
-
-                    
-                </select>
-            </li>
-
-            <input name="add_product" type="submit" id="addToCart" class="btn btn-primary mt-5 w-100" disabled value="Add to cart">
-
-        </form>
-
-    </ul>
-</div>
+<div class="mt-5"></div> <!-- Espace pour la marge -->
 
 <?php
 require_once("inc/footer.php");
